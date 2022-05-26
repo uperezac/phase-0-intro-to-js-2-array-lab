994880ea4b3d9210136198bd1acd3d882582bc79
Ulises Perez Acosta <u.perezacosta@gmail.com>
2022-05-26 01:36:31 -0400

    commit tree 7c0f97ffc87f2de55a2dcd27b0574f5a1dc8cb72

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+//Array of Cats (Jason Deruulo)
+
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(c){
+    cats.push(c)
+}
+function destructivelyPrependCat(c){
+    cats.unshift(c)
+}
+function destructivelyRemoveFirstCat(c){
+    cats.shift(c)
+}
+function destructivelyRemoveLastCat(c){
+    cats.pop(c)
+}
+function destructivelyRemoveFirstCat(c){
+    cats.shift(c)
+}
+function appendCat(c){
+    const copyCats = [...cats]
+    copyCats.push(c)
+    return copyCats
+}
+function prependCat(c){
+    const copyCats = [c, ...cats]
+    return copyCats
+}
+function removeLastCat(){
+    const copyCats = cats.slice(0,2)
+    return copyCats
+}
+function removeFirstCat(){
+    const copyCats = cats.slice(1,3)
+    return copyCats
+}
